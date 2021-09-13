8eccf01bec7c2bf066611f6e02fb18a93b2baf55
Jonathan Dekermanjian <Jonathan.Dekermanjian.CUAnschutz.edu>
2021-09-13 07:33:19 -0600

    fixed unit test

--- tests/testthat/test_MAI.R
+++ tests/testthat/test_MAI.R
@@ -3,9 +3,9 @@ data("untargeted_LCMS_data")
 
 test_that("MAI works properly", {
     result = MAI(untargeted_LCMS_data)
-    expect_true(ncol(result[["Imputed_data"]][["MAI"]])==ncol(untargeted_LCMS_data))
-    expect_true(sum(is.na(result[["Imputed_data"]][["MAI"]]))==0)
-    expect_true(is.numeric(result[["Imputed_data"]][["MAI"]]))
+    expect_true(ncol(result[["Imputed_data"]])==ncol(untargeted_LCMS_data))
+    expect_true(sum(is.na(result[["Imputed_data"]]))==0)
+    expect_true(is.numeric(result[["Imputed_data"]]))
     expect_true(length(result[["Estimated_Params"]])==3)
     expect_true(is.numeric(result[["Estimated_Params"]][["Alpha"]]))
     expect_true(is.numeric(result[["Estimated_Params"]][["Beta"]]))
